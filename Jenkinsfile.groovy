0acc4a137d0fc084dc0d6b1e388af01c9a00d159
Max Goryunov <maximthehedgehog@yandex.ru>
2024-10-10 16:53:15 +0300

    fix

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,65 +1,18 @@
 pipeline {
-    agent any
+       agent any
 
-    stages {
-        stage('Build') {
-            steps {
-                echo 'Building...'
-            }
-        }
-        stage('Download git repo') {
-            steps {
-                echo '===============downloading git repo==================='
-                script {
-                    if (isUnix()) {
-                        sh 'rm -rf tg-reminber-bot'
-                        sh 'git clone --depth=1 https://github.com/MaxGoryunov/tg-reminber-bot.git'
-                        sh 'rm -rf tg-reminber-bot/.git*'
-                    } else {
-                        echo 'not unix'
-                        bat 'powershell $PSVersionTable'
-                        bat 'powershell -Command "Get-ChildItem -Path .\\* -Recurse | Remove-Item -Force -Recurse"'
-                        echo 'removed files'
-                        bat 'git clone  https://github.com/MaxGoryunov/tg-reminber-bot.git'
-                        echo 'cloned repo'
-                        bat 'powershell Remove-Item tg-reminber-bot/.git* -Recurse -Force'
-                        echo 'removed .git file'
-                    }
-                }
-                echo '===============git repo downloaded==================='
-            }
-        }
-        stage('Getting creds and env variables') {
-            steps {
-                echo '===============getting env variables==================='
-                withCredentials([file(credentialsId: 'ENV', variable: 'ENV'), file(credentialsId: 'CREDS', variable: 'CREDS'), file(credentialsId: 'TOKEN', variable: 'TOKEN')]) {
-                    script {
-                        if (isUnix()) {
-                            sh 'cp $ENV ./tg-reminber-bot/.env'
-                            sh 'cp $REDS ./tg-reminber-bot/credentials.json'
-                            sh 'cp $TOKEN ./tg-reminber-bot/token.json'
-                        } else {
-                            bat 'powershell Copy-Item %ENV% -Destination ./tg-reminber-bot/.env'
-                            bat 'powershell Copy-Item %CREDS% -Destination ./tg-reminber-bot/credentials.json'
-                            bat 'powershell Copy-Item %TOKEN% -Destination ./tg-reminber-bot/token.json'
-                        }
-                    }
-                }
-                echo '===============got creds and env variables==================='
-            }
-        }
-    }
-    post {
-        success {
-            echo '===============run docker==================='
-                script {
-                    if (isUnix()) {
-                        sh 'cd tg-reminber-bot && docker-compose up -d --build'
-                    } else {
-                        bat 'cd tg-reminber-bot && docker-compose up -d --build'
-                    }
-                }
-                echo '===============docker container is running successfully==================='
-            }
-        }
-    }
+       stages {
+           stage('Clone Repository') {
+               steps {
+                   git url: 'https://github.com/your-username/your-repo.git', branch: 'main'
+               }
+           }
+           stage('Build and Run Application') {
+               steps {
+                   script {
+                       docker.image('your-docker-image').run('-d -p 8080:8080') // Assuming your Docker image can be run with this command
+                   }
+               }
+           }
+       }
+   }
